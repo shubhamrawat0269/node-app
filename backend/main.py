8737796cc338a75e1cfcc33fc7a11879eb1cb39e
Shubham Rawat <rawatshubam269@gmail.com>
2026-05-28 09:54:30 +0530

    Added : backend integ.

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-
 from collections import defaultdict, deque
+from fastapi.middleware.cors import CORSMiddleware
 
-# creating app
 app = FastAPI()
 
-# create class Edge, Pipeline
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Edge(BaseModel):
     source: str
     target: str
@@ -16,63 +22,51 @@ class Pipeline(BaseModel):
     nodes: list
     edges: List[Edge]
 
-# implementing DAG Check
-
-
 def is_dag(nodes, edges):
 
-  graph = defaultdict(list)
-  indegree = defaultdict(int)
+    graph = defaultdict(list)
+    indegree = defaultdict(int)
 
-  for node in nodes:
-      indegree[node["id"]] = 0
+    for node in nodes:
+        indegree[node["id"]] = 0
 
-  for edge in edges:
-      graph[edge.source].append(
-          edge.target
-      )
+    for edge in edges:
+        graph[edge.source].append(edge.target)
+        indegree[edge.target] += 1
 
-      indegree[edge.target] += 1
+    queue = deque([
+        node
+        for node in indegree
+        if indegree[node] == 0
+    ])
 
-  queue = deque([
-      node
-      for node in indegree
-      if indegree[node] == 0
-  ])
+    visited = 0
 
-  visited = 0
+    while queue:
 
-  while queue:
-      current = queue.popleft()
-      visited += 1
-      for neighbor in graph[current]:
-          indegree[neighbor] -= 1
-          if indegree[neighbor] == 0:
-              queue.append(neighbor)
+        current = queue.popleft()
 
-  return visited == len(nodes)
+        visited += 1
 
-# Create Post cll 
+        for neighbor in graph[current]:
+
+            indegree[neighbor] -= 1
+
+            if indegree[neighbor] == 0:
+                queue.append(neighbor)
+
+    return visited == len(nodes)
+
 @app.post("/pipelines/parse")
 async def parse_pipeline(
-  pipeline: Pipeline
+    pipeline: Pipeline
 ):
 
-  num_nodes = len(
-    pipeline.nodes
-  )
-
-  num_edges = len(
-    pipeline.edges
-  )
-
-  dag_status = is_dag(
-    pipeline.nodes,
-    pipeline.edges
-  )
-
-  return {
-    "num_nodes": num_nodes,
-    "num_edges": num_edges,
-    "is_dag": dag_status,
-  }
+    return {
+        "num_nodes": len(pipeline.nodes),
+        "num_edges": len(pipeline.edges),
+        "is_dag": is_dag(
+            pipeline.nodes,
+            pipeline.edges,
+        ),
+    }
